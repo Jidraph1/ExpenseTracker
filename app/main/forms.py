2b1409ca4b4b439b821163e776576aad93c93545
Jidraph! <jidraph.kimachia@student.moringaschool.com>
2022-05-18 05:50:36 +0300

    custom validators

--- app/main/forms.py
+++ app/main/forms.py
@@ -18,3 +18,10 @@ class RegisterForm(FlaskForm):
     password = PasswordField('Password',validators=[InputRequired()])
 
 
+    def validate_email(self,data_field):
+            if User.query.filter_by(email =data_field.data).first():
+                raise ValidationError('There is an account with that email')
+
+    def validate_username(self,data_field):
+        if User.query.filter_by(username = data_field.data).first():
+            raise ValidationError('That username is taken')
