00bcc4b6dae13796e9079bdfb891554e4c8b3960
Jidraph! <jidraph.kimachia@student.moringaschool.com>
2022-05-20 08:20:32 +0300

    deployment

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
 
 class ProdConfig(Config):
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace(".//", "ql://", 1)
 
 
 class DevConfig(Config):
