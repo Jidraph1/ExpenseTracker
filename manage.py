0ce0db97e7cfd8113910826bc2053328d598675f
Jidraph! <jidraph.kimachia@student.moringaschool.com>
2022-05-20 08:13:20 +0300

    deployment to heroku

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ from flask_script import Shell
 
 
 # Creating app instance
-app = create_app('development')
+app = create_app('production')
 
 manager = Manager(app)
 manager.add_command('server',Server)
